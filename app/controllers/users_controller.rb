8ab01e6a9a363d3216c1683fcd3601411b87220d
PikkaStar <yokotaka1226@gmail.com>
2024-02-02 06:48:27 +0000

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show,:edit,:update]
+
+
   def index
+    @user = current_user
+    @users = User.page(params[:page]).per(10)
   end
 
   def show
@@ -7,4 +12,19 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:introduction,:profile_image)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_one_attached :profile_image
+         validates :name, presence: true
+         validates :introduction, length: {maximum: 100}
+
+         def get_profile_image(width, height)
+           if profile_image.attached?
+             profile_image.variant(resize_to_limit: [width, height]).processed
+           else
+             "no_image"
+           end
+         end
 end
