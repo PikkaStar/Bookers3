c117957a1ee62689ee994652003aa7c57f0b0b24
PikkaStar <yokotaka1226@gmail.com>
2024-02-04 06:25:37 +0000

    js follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          has_many :favorites, dependent: :destroy
          has_many :comments, dependent: :destroy
          has_many :relationships,class_name: "Relationship",foreign_key: "follower_id"
-         has_many :reverse_of_relationships,class_name: "Relationshop",foreign_key: "follower_id"
+         has_many :reverse_of_relationships,class_name: "Relationship",foreign_key: "followed_id"
          has_many :followings, through: :relationships, source: :followed
          has_many :followers, through: :reverse_of_relationships,source: :follower
          validates :name, presence: true
