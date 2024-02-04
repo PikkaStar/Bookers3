93a2df4e5176c50140501849215ea40ffe277b0a
PikkaStar <yokotaka1226@gmail.com>
2024-02-04 05:03:05 +0000

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
          has_many :books,dependent: :destroy
          has_many :favorites, dependent: :destroy
          has_many :comments, dependent: :destroy
+         has_many :relationships,class_name: "Relationship",foreign_key: "follower_id"
+         has_many :reverse_of_relationships,class_name: "Relationshop",foreign_key: "follower_id"
+         has_many :followings, through: :relationships, source: :followed
+         has_many :followers, through: :reverse_of_relationships,source: :follower
          validates :name, presence: true
          validates :introduction, length: {maximum: 100}
 
@@ -16,5 +20,17 @@ class User < ApplicationRecord
            else
              "no_image"
            end
+         end
+
+         def follow(user)
+           relationships.create(followed_id: user.id)
+         end
+
+         def unfollow(user)
+           relationships.find_by(followed_id: user.id).destroy
+         end
+
+         def following?(user)
+           followings.include?(user)
          end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "homes#top"
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings"=>"relationships#followings",as: "followings"
+    get "followers"=>"relationships#followers",as: "followers"
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
